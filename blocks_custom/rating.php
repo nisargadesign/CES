cafc94915193cfcd25657827ebac853acf67ba97
Vital <vital@fineonly.com>
2013-02-27 14:13:39 -0800

    Update also the item table rating fields

--- blocks_custom/rating.php
+++ blocks_custom/rating.php
@@ -3,6 +3,7 @@ if(isset($_REQUEST['id']) && isset($_REQUEST['rating']) && is_numeric($_REQUEST[
 	include_once("../includes/var_definition.php");
 	include_once("../includes/constants.php");
 	include_once("../includes/db_$db_lib.php");
+	include_once("../includes/common_functions.php");
 
 	$db = new VA_SQL();
 	$db->DBType      = $db_type;
@@ -13,8 +14,12 @@ if(isset($_REQUEST['id']) && isset($_REQUEST['rating']) && is_numeric($_REQUEST[
 	$db->DBPassword  = $db_password;
 	$db->DBPersistent= $db_persistent;
 
-	
-        $sql = "INSERT INTO `va_reviews` (`review_id`, `item_id`, `user_id`, `admin_id`, `recommended`, `approved`, `rating`, `summary`, `user_name`, `user_email`, `remote_address`, `comments`, `admin_id_approved_by`, `admin_id_modified_by`, `date_added`, `date_modified`, `date_approved`) VALUES (NULL, '".$_REQUEST['id']."', '0', '0', '1', '1', '".$_REQUEST['rating']."', 'Web', 'Visitor', 'visitor@cuttingedgestencils.com', '".$_SERVER['REMOTE_ADDR']."', 'Fantastic product', NULL, NULL, NOW(), NOW(), NOW())";
+	$product_id = $db->tosql($_REQUEST['id'], INTEGER);
+        $sql = "INSERT INTO `va_reviews` (`review_id`, `item_id`, `user_id`, `admin_id`, `recommended`, `approved`, `rating`, `summary`, `user_name`, `user_email`, `remote_address`, `comments`, `admin_id_approved_by`, `admin_id_modified_by`, `date_added`, `date_modified`, `date_approved`) VALUES (NULL, '".$product_id."', '0', '0', '1', '1', '".$db->tosql($_REQUEST['rating'], INTEGER)."', 'Web', 'Visitor', 'visitor@cuttingedgestencils.com', '".$_SERVER['REMOTE_ADDR']."', 'Fantastic product', NULL, NULL, NOW(), NOW(), NOW())";
+	$db->query($sql);
+	$votes = get_db_value("SELECT COUNT(*) FROM ".$table_prefix."reviews WHERE approved=1 AND rating <> 0 AND item_id=".$product_id);
+	$points = get_db_value("SELECT SUM(rating) FROM ".$table_prefix."reviews WHERE approved=1 AND rating <> 0 AND item_id=".$product_id);
+	$sql = "UPDATE ".$table_prefix."items SET votes=".$votes.", points=".$points." WHERE item_id=".$product_id;
 	$db->query($sql);
 }
 else {
